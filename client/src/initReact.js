d7067e5bf8987d489b8d8840a7e199bee0923ccb
harisstavrinos <haris.stavrinos@gmail.com>
2020-01-06 22:01:08 +0200

    #2 Initialisation - React init without Express

---
+++ client/src/initReact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function InitReact() {
+  return (
+    <div className="InitReact">
+        <h1>Hello Guys</h1> <br/>
+        That is just a test to initiate React <br/>
+        For the moment node_module and package.json are used independently in client folder,<br/>
+        as used in a React project without Expressjs        
+    </div>
+  );
+}
+
+export default InitReact;
