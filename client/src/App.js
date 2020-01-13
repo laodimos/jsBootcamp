ba2b41d8801a34a524827a2b0671a059d502c8de
harisstavrinos <haris.stavrinos@gmail.com>
2020-01-13 22:44:34 +0200

    #2 Initialisation - step 4: adding pg project in server

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,6 @@ import React, {Component} from 'react';
 import { render } from '@testing-library/react';
 
 class App extends Component {
-  state = {
-    hello: null
-  }
-
-
   render() {
     return (
       <div>
